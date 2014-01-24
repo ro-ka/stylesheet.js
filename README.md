cdcb6d9eea0d72d0f5d2b038291e96d06f2654fb
Robert Katzki <katzki@ubilabs.net>
2014-01-24 11:05:33 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,70 @@
+# stylesheet.js [![Build Status](https://secure.travis-ci.org/ro-ka/stylesheet.js.png?branch=master)](http://travis-ci.org/ro-ka/stylesheet.js)
+
+Generate and edit a dynamic Cascading Style Sheet using JavaScript.
+
+## Getting Started
+Install the script via [bower](http://bower.io/):
+
+```
+bower install stylesheet.js --save
+```
+
+Include it in the head of your page:
+
+```html
+…
+<head>
+  …
+  <script src="/bower_components/stylesheet.js/dist/stylesheet.min.js"></script>
+  …
+</head>
+…
+```
+
+You are now ready to go!
+
+## Documentation
+
+Generate a new Instance:
+
+```js
+var stylesheet = new Stylesheet();
+```
+
+### #addRule
+
+You can add new rules to that created stylesheet. It takes 3 parameters:
+* *required* String `selector` The CSS selector.
+* *required* String `rules` The CSS rules.
+* *optional* Number `index` An index, where to insert the rule at in the new stylesheet.
+
+### #deleteRule
+
+To delete a rule, pass in the selector of that rule.
+* *required* String `selector` The CSS selector.
+
+### #clear
+
+This will clear the whole stylesheet, leaving it empty without any rules.
+
+## Examples
+
+```js
+var stylesheet = new Stylesheet();
+
+stylesheet.addRule('.ninja', 'visibility: hidden;'); // Add a new rule
+stylesheet.addRule('.bear', 'color: brown;', 0); // Add as first rule
+
+stylesheet.deleteRule('.ninja'); // Delete the .ninja rule
+
+stylesheet.clear(); // Clear all rules
+```
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+1.0.0 Initial release
+
+## License
+Copyright (c) 2014 Robert Katzki. Licensed under the MIT license.
---
+++ test/stylesheet-test.js
@@ -0,0 +1,139 @@
+/* global chai, beforeEach, describe, it, Stylesheet, CSSStyleSheet */
+
+var expect = chai.expect;
+
+describe('Stylesheet', function() {
+  var stylesheet;
+
+  beforeEach(function() {
+    stylesheet = new Stylesheet();
+  });
+
+  describe('constructor', function() {
+    it('creates a new Stylesheet', function() {
+      expect(stylesheet).to.be.an.instanceof(Stylesheet);
+    });
+
+    it('adds a new element to the page', function() {
+      var style = document.getElementsByTagName('style')[0];
+
+      expect(style).to.exist;
+    });
+
+    it('adds a new element to the document.styleSheets', function() {
+      var stylesBefore = document.styleSheets.length,
+        newStylesheet = new Stylesheet(),
+        stylesAfter = document.styleSheets.length;
+
+      expect(stylesAfter).to.equal(stylesBefore + 1);
+    });
+
+    it('creates an empty CSSStyleSheet', function() {
+      expect(stylesheet.sheet.rules.length).to.equal(0);
+    });
+
+    it('has an internal reference to the created CSSStyleSheet', function() {
+      expect(stylesheet.sheet).to.be.an.instanceof(CSSStyleSheet);
+    });
+  });
+
+  describe('#addRule', function() {
+    beforeEach(function() {
+      stylesheet.clear();
+    });
+
+    it('adds a new rule to the stylesheet', function() {
+      stylesheet.addRule('.ninja', 'visibility: hidden;');
+
+      expect(stylesheet.sheet.rules.length).to.equal(1);
+    });
+
+    it('throws an error when no selector is passed in', function() {
+      expect(function() {
+        stylesheet.addRule();
+      }).to.throw(Error);
+    });
+
+    it('throws an error when the selector is not a string', function() {
+      expect(function() {
+        stylesheet.addRule(12, 'visibility: hidden;');
+      }).to.throw(Error);
+    });
+
+    it('throws an error when the selector is an empty string', function() {
+      expect(function() {
+        stylesheet.addRule('', 'visibility: hidden;');
+      }).to.throw(Error);
+    });
+
+    it('throws an error when no rule is passed in', function() {
+      expect(function() {
+        stylesheet.addRule('.ninja');
+      }).to.throw(Error);
+    });
+
+    it('throws an error when rule is not a string', function() {
+      expect(function() {
+        stylesheet.addRule('.ninja', 12);
+      }).to.throw(Error);
+    });
+
+    it('throws an error when rule is an empty string', function() {
+      expect(function() {
+        stylesheet.addRule('.ninja', '');
+      }).to.throw(Error);
+    });
+
+    it('throws an error when index is not a number', function() {
+      expect(function() {
+        stylesheet.addRule('.ninja', 'visibility: hidden;', '');
+      }).to.throw(Error);
+    });
+  });
+
+  describe('#deleteRule', function() {
+    beforeEach(function() {
+      stylesheet.clear();
+    });
+
+    it('deletes a rule from the stylesheet', function() {
+      stylesheet.addRule('.ninja', 'visibility: hidden;');
+      stylesheet.addRule('.bear', 'color: brown;');
+      stylesheet.deleteRule('.ninja');
+
+      expect(stylesheet.sheet.rules.length).to.equal(1);
+    });
+
+    it('throws an error when no selector is passed in', function() {
+      expect(function() {
+        stylesheet.deleteRule();
+      }).to.throw(Error);
+    });
+
+    it('throws an error when the selector is not a string', function() {
+      expect(function() {
+        stylesheet.deleteRule(12);
+      }).to.throw(Error);
+    });
+
+    it('throws an error when the selector is an empty string', function() {
+      expect(function() {
+        stylesheet.deleteRule('');
+      }).to.throw(Error);
+    });
+  });
+
+  describe('#clear', function() {
+    beforeEach(function() {
+      stylesheet.clear();
+    });
+
+    it('clears the whole stylesheet', function() {
+      stylesheet.addRule('.ninja', 'visibility: hidden;');
+      stylesheet.addRule('.bear', 'color: brown;');
+      stylesheet.clear();
+
+      expect(stylesheet.sheet.rules.length).to.equal(0);
+    });
+  });
+});
